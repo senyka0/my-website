1b5fb706e3df39a2fb6fb0ec359ea9dbd95c2853
senyka0 <arsk293@gmail.com>
2026-04-09 01:39:21 +0300

    feat(frontend): add favicon

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -49,6 +49,11 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
+  icons: {
+    icon: "/icon.svg",
+    shortcut: "/icon.svg",
+    apple: "/icon.svg",
+  },
 };
 
 export const viewport: Viewport = {
